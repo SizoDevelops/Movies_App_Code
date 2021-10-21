24c2401780db86b04c49113583f352fc1b4d3b1b
FierceInc <fierceincii@gmail.com>
2021-10-21 10:28:37 +0200

    First Movies App Deploy

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const moviesRouter = require('./routes/movies')
+app.use(express.static(__dirname));
+app.set("view engine", "ejs");
+app.use('/', moviesRouter)
+app.listen(process.env.PORT || 3000 , () => {
+    console.log("Listening to port")
+})
