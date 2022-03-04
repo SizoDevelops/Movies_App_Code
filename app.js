70c89ab67361c1c2acc8614d5b5a366b8a65b5e8
FierceInc <fierceincii@gmail.com>
2022-03-04 14:31:26 +0200

    Re deploy

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ const moviesRouter = require('./routes/movies')
 app.use(express.static(__dirname));
 app.set("view engine", "ejs");
 app.use('/movies', moviesRouter)
-app.listen(process.env.PORT || 4000 , () => {
+app.listen(process.env.PORT || 5000 , () => {
     console.log("Listening to port")
-})
+}) 
