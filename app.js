4cb707b9c69e7d3213ec0d78e022f018d3c76342
FierceInc <fierceincii@gmail.com>
2022-03-05 11:44:42 +0200

    added Icons

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const moviesRouter = require('./routes/movies')
 app.use(express.static(__dirname));
 app.set("view engine", "ejs");
-app.use('/movies', moviesRouter)
+app.use("/", moviesRouter)
 app.listen(process.env.PORT || 5000 , () => {
     console.log("Listening to port")
 }) 
